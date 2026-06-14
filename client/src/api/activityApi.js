0460fa79bd20d9c5d84e23d4348240193675528f
dakshumeena <152151107+dakshumeena@users.noreply.github.com>
2026-06-14 06:55:48 +0530

    Delete client directory

--- client/src/api/activityApi.js
+++
@@ -1,6 +0,0 @@
-import API from "./axios";
-
-export const getWorkspaceActivities = async (workspaceId) => {
-  const res = await API.get(`/activities/workspace/${workspaceId}`);
-  return res.data;
-};
